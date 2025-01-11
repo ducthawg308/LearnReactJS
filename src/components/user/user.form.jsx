74f59c676566c4d651d9ca5513d117f05a3dd8c5
_ducthawg.04 <you@example.com>
2025-01-12 02:25:39 +0700

    cai axios

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+
+const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const handleAddUser = () => {
+        alert(`Full Name: ${fullName}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}`);
+    }
+
+    return (
+        <form className="max-w-sm mx-auto">
+            <div className="mb-5">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                <input onChange={(event)=>{setFullName(event.target.value)}} value={fullName} type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nguyen Van A" required />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input onChange={(event)=>{setEmail(event.target.value)}} value={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input onChange={(event)=>{setPassword(event.target.value)}} value={password} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
+                <input onChange={(event)=>{setPhone(event.target.value)}} value={phone} type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <button onClick={()=>{handleAddUser()}} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create User</button>
+        </form>
+    )
+}
+
+export default UserForm;
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,9 +1,12 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
 
 
 const UserPage = () => {
     return (
-        <div>
-            <h1>User Page</h1>
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <UserForm />
+            <UserTable />
         </div>
     );
 }
